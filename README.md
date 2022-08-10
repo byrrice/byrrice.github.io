a3c9f680befb1912d57327a6fe303eaf6f6f4b68
byrrice <32532726+byrrice@users.noreply.github.com>
2022-08-10 00:31:22 -0500

    we have done it

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Isaac's website
 
 - To instasll, npm run deploy
+- https://www.youtube.com/watch?v=Ht8Onfr3AR8
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,19 @@
 import React from "react";
 import cat from "../images/cat.png";
+import { analytics } from "../firebase-config"
+import { logEvent} from "firebase/analytics";
 
 export default function Contact() {
+
+  const logCatClick = () => {
+    console.log("CAT CLICKER IDENTIFIED");
+    logEvent(analytics, "stop clicking the cat");
+  }
+
   return (
     <div className="isaac">
-        <img src={cat}/>
+      <h2 id="title">CLICK ME PLS</h2>
+      <img src={cat} onClick={logCatClick}/>
     </div>
   );
 }
